bd79ecb361e88e1d92e53c58d711fb0cf262c82d
Vincent Siebert <vincent@siebert.im>
2012-08-13 16:22:39 +0100

    updating middleman aliases

--- zsh/after/alias.after.zsh
+++ zsh/after/alias.after.zsh
@@ -3,5 +3,5 @@ alias gmn='git merge --no-ff'
 
 # Middleman Aliases
 alias mmi='middleman init'
-alias mms='middleman server'
-alias mmb='middleman build'
+alias mms='bundle exec middleman server'
+alias mmb='bundle exec middleman build'
